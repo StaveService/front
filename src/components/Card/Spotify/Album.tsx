7613381ca2caf04e6ed24e15e74e06106e0ad006
izszzz <izszzz@yahoo.co.jp>
2021-06-24 11:04:55 +0900

    fix warning

--- src/components/Card/Spotify/Album.tsx
+++ src/components/Card/Spotify/Album.tsx
@@ -1,25 +1,16 @@
 import React from "react";
 import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
 import Box from "@material-ui/core/Box";
-import { makeStyles } from "@material-ui/core/styles";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import { ISpotifyAlbum, ISpotifyArtist } from "../../../interfaces";
+import { ISpotifyAlbum } from "../../../interfaces";
 
-const useStyles = makeStyles({
-  media: {
-    height: 100,
-    width: 100,
-  },
-});
 interface ISpotifyAlbumCard {
   album: ISpotifyAlbum;
 }
 const Album: React.FC<ISpotifyAlbumCard> = ({
   album: { name },
 }: ISpotifyAlbumCard) => {
-  const classes = useStyles();
   return (
     <Card>
       <Box display="flex">
--- src/components/Card/Spotify/Artist.tsx
+++ src/components/Card/Spotify/Artist.tsx
@@ -1,25 +1,16 @@
 import React from "react";
 import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
 import Box from "@material-ui/core/Box";
-import { makeStyles } from "@material-ui/core/styles";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import { ISpotifyArtist } from "../../../interfaces";
 
-const useStyles = makeStyles({
-  media: {
-    height: 100,
-    width: 100,
-  },
-});
 interface ISpotifyArtistCard {
   artist: ISpotifyArtist;
 }
 const SpotifyTrackCard: React.FC<ISpotifyArtistCard> = ({
   artist: { name },
 }: ISpotifyArtistCard) => {
-  const classes = useStyles();
   return (
     <Card>
       <Box display="flex">
