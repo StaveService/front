397b99ffec4cd9e8e4fe74b776cf6d31e6d49da5
izszzz <izszzz@yahoo.co.jp>
2021-07-20 17:02:34 +0900

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+import { SnackbarProvider } from "notistack";
 import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { SnackbarProvider } from "notistack";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { store, persistor } from "./store";
 import Routes from "./Routes";
