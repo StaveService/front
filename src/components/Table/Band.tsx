7c30070f9096585fe11415295e9a66711413d5c6
izszzz <izszzz@yahoo.co.jp>
2021-08-16 00:04:10 +0900

    update band_table

--- src/components/Table/Band.tsx
+++ src/components/Table/Band.tsx
@@ -6,6 +6,8 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Link from "@material-ui/core/Link";
+import TwitterIcon from "../Icon/Twitter";
+import LinkIconButton from "../Button/Icon/Link";
 import { IBand } from "../../interfaces";
 import routes from "../../constants/routes.json";
 import Layout, { LayoutProps } from "./Layout";
@@ -39,6 +41,17 @@ const Band: React.FC<BandProps> = ({
                 <Link component={RouterLink} to={`${routes.BANDS}/${band.id}`}>
                   {band.name}
                 </Link>
+              </TableCell>
+              <TableCell>
+                <LinkIconButton
+                  href={
+                    band.link.twitter
+                      ? `https://twitter.com/${band.link.twitter}`
+                      : undefined
+                  }
+                >
+                  <TwitterIcon />
+                </LinkIconButton>
               </TableCell>
             </TableRow>
           ))}
