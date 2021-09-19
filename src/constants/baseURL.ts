74c926365fe688192e1b8e3699c1af4e201090e8
izszzz <izszzz@yahoo.co.jp>
2021-09-19 19:46:22 +0900

    fix baseurl

--- src/constants/baseURL.ts
+++ src/constants/baseURL.ts
@@ -1,6 +1,6 @@
 const baseURL =
   process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"
     ? "http://localhost"
-    : "http://http://stave-back.com/";
+    : "http://stave-back.com";
 
 export default baseURL;
